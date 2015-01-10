43d94842c1430185b27dcbcaeb30ba8deee31226
andy <dephiros@gmail.com>
2015-01-10 00:02:13 -0800

    create a webview and progress bar in mainActivity

--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -2,11 +2,17 @@ package com.commonsware.empublite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class EmPubLiteActivity extends Activity {
+    WebView browser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        browser = (WebView)findViewById(R.id.webView);
+        browser.setWebViewClient(new WebViewClient());
+        browser.loadUrl("http://www.dephiros.com");
     }
 }
