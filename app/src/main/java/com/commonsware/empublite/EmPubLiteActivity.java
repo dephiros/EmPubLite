a86a1ae31aa5d3c4e8710f51595b3d585e2fe36b
lyle <lylechevrette@gmail.com>
2015-01-12 23:40:03 -0800

    import eventbus, gson, retrofit, support lib add dummy about, home, help menu item

--- app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
+++ app/src/main/java/com/commonsware/empublite/EmPubLiteActivity.java
@@ -2,6 +2,8 @@ package com.commonsware.empublite;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -14,5 +16,24 @@ public class EmPubLiteActivity extends Activity {
         browser = (WebView)findViewById(R.id.webView);
         browser.setWebViewClient(new WebViewClient());
         browser.loadUrl("http://www.dephiros.com");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return (super.onCreateOptionsMenu(menu));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                return (true);
+            case R.id.about:
+                return (true);
+            case R.id.help:
+                return (true);
+        }
+        return (super.onOptionsItemSelected(item));
     }
 }
