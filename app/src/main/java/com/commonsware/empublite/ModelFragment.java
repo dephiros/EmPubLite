b42d4f9360f7321acd134549ff7cb36548223760
Andy <dephiros@gmail.com>
2015-02-08 18:56:44 -0800

    Added BookContents and ModelFragment for holding the book

---
+++ app/src/main/java/com/commonsware/empublite/ModelFragment.java
@@ -0,0 +1,12 @@
+package com.commonsware.empublite;
+
+import android.app.Fragment;
+
+import java.util.List;
+
+/**
+ * Created by lyan on 2/8/15.
+ */
+public class ModelFragment extends Fragment {
+
+}
